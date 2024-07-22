c56393dae5345c6508b4d69e3e68b7577b0960eb
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-07-22 16:34:54 +0300

    Time: 42 ms (17.41%) | Memory: 16.4 MB (80.47%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,5 +1,7 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
+        # it's diffcult to do in cpp
+        # need to immplement function by my self
         odd = n//2
         even = n-odd
         mod = 1000000007
