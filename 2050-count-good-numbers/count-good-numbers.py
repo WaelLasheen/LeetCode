39d933c1656a21d30acde7a16eed26d6fc17146a
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-07-22 16:38:03 +0300

    Delete 2050-count-good-numbers directory

--- 2050-count-good-numbers/count-good-numbers.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countGoodNumbers(self, n: int) -> int:
-        # it's diffcult to do in cpp
-        # need to immplement function by my self
-        odd = n//2
-        even = n-odd
-        mod = 1000000007
-        return (pow(5,even,mod)*pow(4,odd,mod))%mod
