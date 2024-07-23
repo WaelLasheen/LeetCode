2791e24b5bc93fb9e4d19532ba82a9cd2da4d4d6
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-07-23 09:14:54 +0300

    Time: 11 ms (38.83%) | Memory: 20.8 MB (33.95%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -5,32 +5,29 @@
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x, left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x, left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int> largestValues(TreeNode* root) {
-        if (!root) return {};
-        
-        queue<TreeNode*> q;
-        vector<int> res;
-        
-        q.push(root);
-        while (!q.empty()) {
-            int v = INT_MIN;
-            int size = q.size(); // Get the current level size
-            for (int i = 0; i < size; ++i) {
-                TreeNode* node = q.front();
-                q.pop();
-                v = max(v, node->val);
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            res.push_back(v);
+    vector<int> res;
+
+    void dfs(TreeNode* root ,int i){
+        if(!root) return;
+
+        if(res.size() >= i+1){
+            res[i] = max(res[i],root->val);
+        }else{
+            res.push_back(root->val);
         }
 
+        dfs(root->left ,i+1);
+        dfs(root->right ,i+1);
+    }
+
+    vector<int> largestValues(TreeNode* root) {
+        dfs(root ,0);
         return res;
     }
 };
