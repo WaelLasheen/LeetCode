1f539249472968e61ecb42043b6a909c47b45170
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-07-24 01:34:26 +0300

    Time: 0 ms (100.00%) | Memory: 8.3 MB (13.03%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long i=0;
-        for( ;i*i<x;i++){
-            if(x> i*i && x<(i+1)*(i+1)){
-                return i;
+        long l=0 ,r=x;
+        while(l<=r){
+            long mid = (l+r)/2;
+            if(mid*mid <= x){
+                l = mid +1;
+            }else{
+                r = mid-1;
             }
         }
-
-        return i;
+        return l-1;
     }
 };
