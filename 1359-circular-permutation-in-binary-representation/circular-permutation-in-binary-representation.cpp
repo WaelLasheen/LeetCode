f655eefab7ad4a4377e6a251e4edb1e4f3e58cf2
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-07-24 13:29:15 +0300

    Time: 357 ms (5.24%) | Memory: 46.3 MB (5.62%) - LeetSync

--- 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.cpp
+++ 1359-circular-permutation-in-binary-representation/circular-permutation-in-binary-representation.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    vector<int> circularPermutation(int n, int start) {
-        // n=2
-        // 0 1 3 2  >>>  3 2 0 1
-        int s=1<<n;
-        vector<int> res(s);
-        for(int i=0;i<s;i++){
-            res[i]= i^(i>>1);   // i xor i/2
+    vector<int> res;
+    vector<bool> v;
+    void solve(int curr, int n){
+        if(res.size()== (1<<n)){
+            int x=res[0]^res[(1<<n) -1];
+            int c=0;
+            while(x){
+                c +=(x&1);
+                x >>=1;
+            }
+            if(c==1) return;
         }
-        auto it = find(res.begin(),res.end(),start);
-        reverse(res.begin() ,it);
-        reverse(it, res.end());
-        reverse(res.begin() ,res.end());
 
+        if(!(v[curr])){
+            res.push_back(curr);
+            v[curr]=true;
+            for(int i=0;i<n;i++){
+                int next = curr ^(1<<i);
+                if(!(v[next])){
+                    solve(next,n);
+                }
+            }
+        }
+    }
+
+    vector<int> circularPermutation(int n, int start) {
+        v.resize(1<<n,false);
+        solve(start,n);
         return res;
     }
 };
