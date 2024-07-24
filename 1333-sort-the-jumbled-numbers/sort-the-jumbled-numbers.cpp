5df26fb7a04f35a4ef9331e9afa885b5f299b049
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-07-24 04:27:58 +0300

    Time: 290 ms (57.58%) | Memory: 110.5 MB (31.17%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        map<int,vector<int>> newNums;
+        multimap<int,int> newNums;
         int s =nums.size();
-        // int p=0;
         for(int i:nums){
-            int x=0 ,n=i;
+            int x=0;
             // if num is from one digit we do it away
-            if(n<10){
-                newNums[mapping[n]].push_back(i);
-                // nums[p]=x;
-                // p++;
+            if(i<10){
+                newNums.insert(pair<int,int>(mapping[i],i));
                 continue;
             }
             string num= to_string(i);
             for(char c:num){
                 x = x*10+mapping[(c-'0')];
-                n /=10;
             }
 
-            newNums[x].push_back(i);
-            // nums[p]=x;
-            // p++;
+            newNums.insert(pair<int,int>(x,i));
         }
         int p=0;
         for(auto i:newNums){
-            for(int x:i.second){
-                nums[p]=x;
-                p++;
-            }
+            nums[p]=i.second;
+            p++;
         }
         return nums;
     }
