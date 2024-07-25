53653f062478fea3a3ec65c2e3c3bb55d97fa54b
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-07-25 08:07:24 +0300

    Time: 139 ms (58.84%) | Memory: 75.5 MB (52.85%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        map<int,int>fr;
-        for(int i:nums) fr[i]++;
-        nums.clear();
-        for(auto i:fr){
-            while(i.second--){
-                nums.push_back(i.first);
-            }
+        priority_queue<int>pq;
+        for(int i:nums){
+            pq.push(i);
+        }
+        for(int i=nums.size()-1;i>-1;i--){
+            nums[i]=pq.top();
+            pq.pop();
         }
         return nums;
     }
