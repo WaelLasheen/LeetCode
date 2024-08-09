b13c747e963d1078435707c8036fe75cc90704f3
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-08-09 08:26:47 +0300

    Time: 6 ms (21.43%) | Memory: 14.2 MB (7.14%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int res=0;
+        int row = grid.size() ,col=grid[0].size();
+        if(row <3 || col <3) return res;
+
+        for(int i=0;i<row-2;i++){
+            for(int j=0;j<col-2;j++){
+                map<int,int> sumr,sumc;
+                map<int,int> fr;
+                int d1=0 ,d2=0;
+                for(int a=0;a<3;a++){
+                    for(int b=0;b<3;b++){
+                        sumr[a] +=grid[i+a][b+j];
+                        sumc[b] +=grid[i+a][b+j];
+
+                        if(a==b)   d1 +=grid[i+a][b+j];
+                        if(a+b==2) d2 +=grid[i+a][b+j];
+
+                        fr[grid[i+a][b+j]]++;
+                    }
+                }
+
+                if (sumr == sumc && sumr[0] == sumr[1] && sumr[1] == sumr[2] && sumr[0] == d1 && sumr[0] == d2 && fr.size() == 9) {
+                    for(int n=1;n<10;n++){
+                        if(!fr[n]){
+                            res--;
+                            break;
+                        }
+                    }
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
