e06dc343f1e1a4c1a5ad6374fb99ff9811cd6caa
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-08-12 06:37:40 +0300

    Time: 27 ms (51.98%) | Memory: 24.9 MB (17.04%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,36 +1,24 @@
+#include <queue>
+#include <vector>
+
 class KthLargest {
 public:
-    vector<int> largek;
-    KthLargest(int k, vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        if(nums.size() < k){
-            largek.push_back(INT_MIN);
-            for(int i=nums.size()-k+1;i<nums.size();i++){
-                largek.push_back(nums[i]);
-            }
-            return;
-        }
-        
-        for(int i=nums.size()-k;i<nums.size();i++){
-            largek.push_back(nums[i]);
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;  // Min-heap
+
+    KthLargest(int k, vector<int>& nums) : k(k) {
+        for (int num : nums) {
+            add(num);
         }
     }
     
     int add(int val) {
-        if(!largek.size()){
-            largek.push_back(val);
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
         }
-        else if(val > largek[0]){
-            largek[0]= val;
-            sort(largek.begin(),largek.end());
-        }
-
-        return largek[0];
+        return minHeap.top();
     }
 };
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
