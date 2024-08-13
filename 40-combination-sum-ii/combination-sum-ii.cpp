0c2fcfd556b4c308e208b32457686b4502a6535f
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-08-13 13:29:53 +0300

    Time: 0 ms (100.00%) | Memory: 13.2 MB (32.40%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        set<pair<int,vector<int>>> con;
-        vector<int> em; // just empty array you don't need it.
-        con.insert({0,em});
-        for(int i:candidates){
-            set<pair<int,vector<int>>> inner = con;
-            for(pair<int,vector<int>> j: con){
-                vector<int> arr = j.second;
-                if(j.first+i <= target){
-                    arr.push_back(i);
-                    inner.insert({j.first+i,arr});
-                }else{
-                    break;
-                }
-            }
-            con = inner;
-        }
-        vector<vector<int>> res;
-        for(pair<int,vector<int>> i: con){
-            if(i.first==target){
-                res.push_back(i.second);
-            }
-        }
+    vector<vector<int>> res;
 
+    void solve(vector<int>& candidates, int target, int index, vector<int>& sub) {
+        if (target == 0) {
+            res.push_back(sub);
+            return;
+        }
+        
+        for (int i = index; i < candidates.size(); i++) {
+            // Skip duplicates
+            if (i > index && candidates[i] == candidates[i-1]) continue;
+            // Early termination
+            if (candidates[i] > target) break;
+            
+            sub.push_back(candidates[i]);
+            solve(candidates, target - candidates[i], i + 1, sub);
+            sub.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> sub;
+        solve(candidates, target, 0, sub);
         return res;
     }
 };
