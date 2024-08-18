80b04c64d1acaa9961ddbe0075aa7b55ba039d29
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-08-18 07:29:07 +0300

    Time: 824 ms (5.05%) | Memory: 136.8 MB (5.10%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,31 @@
+#define ull unsigned long long
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        set<unsigned long long> ugly={1,2,3,5};
+        int t=8;
+        while(t--){
+            int c=ugly.size();
+            ull fac=5;
+            for(ull i:ugly){
+                ugly.insert(i*fac);
+                if(c-- ==0) break;
+            }
+            c=ugly.size();
+            fac=3;
+            for(ull i:ugly){
+                ugly.insert(i*fac);
+                if(c-- ==0) break;
+            }
+            c=ugly.size();
+            fac=2;
+            for(ull i:ugly){
+                ugly.insert(i*fac);
+                if(c-- ==0) break;
+            }
+        }
+        vector<ull> arr(ugly.begin(),ugly.end());
+        return arr[n-1];
+    }
+};
