8545f65911d6be4ed08dbde5038b0d299631111a
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-08-31 05:28:51 +0300

    Time: 148 ms (17.57%) | Memory: 76.6 MB (6.49%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        unordered_map<int,vector<pair<int,double>>> adj;
+        for(int i=0 ;i< edges.size();i++){
+            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+        }
+
+        priority_queue<pair<double,int>> pq;
+        pq.push({1.0,start});
+        unordered_set<int> visit;
+
+        while(pq.size()){
+            double prob = pq.top().first;
+            int cur = pq.top().second;
+            pq.pop();
+
+            if(cur == end){
+                return prob;
+            }
+
+            // to skip unnecessary loop
+            if (visit.find(cur) != visit.end()) {
+                continue;
+            }
+
+            visit.insert(cur);
+            
+            for(pair<int,double> node : adj[cur]){
+                if(visit.find(node.first) == visit.end()){
+                    pq.push({prob * node.second , node.first});
+                }
+            }
+        }
+
+        return 0;
+    }
+};
