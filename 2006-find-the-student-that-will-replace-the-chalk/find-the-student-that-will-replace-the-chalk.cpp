28a922e4e1eb01139be4977190e4804295c229e0
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-02 08:01:43 +0300

    Time: 1191 ms (5.04%) | Memory: 83.8 MB (5.65%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        if(chalk.size()==1) return 0;
+        if(chalk.size()==1 || chalk[0]>k) return 0;
 
         int s=chalk.size();
         vector<long long> arr(chalk.begin(),chalk.end());
         for(int i=1;i<s;i++){
             arr[i] += arr[i-1];
+            if(arr[i] > k) return i;
         }
 
         int ind=0;
