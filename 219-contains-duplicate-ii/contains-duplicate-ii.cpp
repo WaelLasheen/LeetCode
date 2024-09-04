db0ae342d24f410813db18915efaf040e3b0ff9a
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-04 22:49:29 +0300

    Time: 163 ms (25.98%) | Memory: 84.4 MB (14.05%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         if(!k) return false;
-        map<int,vector<int>> num_it;
+        map<int,int> num_it;
         for(int i=0;i<nums.size();i++){
-            num_it[nums[i]].push_back(i);
+            if(num_it.find(nums[i]) != num_it.end() && i-num_it[nums[i]] <= k){
+                return true;
+            }
+
+            num_it[nums[i]]=i;
         }
 
-        for(auto it:num_it){
-            if(it.second.size()>1){
-                for(int i=0;i<it.second.size()-1;i++){
-                    if(it.second[i+1] - it.second[i] <=k){
-                        return true;
-                    }
-                }
-            }
-        }
 
         return false;
     }
