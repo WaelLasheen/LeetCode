2a8b3298dbe0ebe66fba527f18436d10ac2f8679
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-04 23:13:01 +0300

    Time: 2 ms (77.07%) | Memory: 7.5 MB (52.79%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int s= high-low+1;
-        if(!(s&1)) return s/2;
-        if(low&1) return s/2+1;
-        return s/2;
+        int len = high-low +1;
+        // if len is even this mean half is odd and half is even
+        if(!(len&1)){
+            return len/2;
+        }
+        // if len is odd this mean there is different by 1 between even and odd
+        // and this depend on the start
+        if(low&1){
+            return len/2+1;
+        }
+
+        return len/2;
     }
 };
