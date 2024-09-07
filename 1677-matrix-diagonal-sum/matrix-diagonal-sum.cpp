71ea356e17fb72d30eb482569b07244cba815f50
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-07 21:56:41 +0300

    Time: 15 ms (17.29%) | Memory: 14.1 MB (37.87%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int s=mat.size();
+        int res=0;
+        for(int i=0;i<s;i++){
+            // primary diagonal
+            res +=mat[i][i];
+            // secondary  diagonal
+            res +=mat[i][s-i-1];
+        }
+
+        if(s&1) res -=mat[s/2][s/2];
+        return res;
+    }
+};
