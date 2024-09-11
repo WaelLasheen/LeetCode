bf03e99ff064c4892e720395e7cda3d02a4396c4
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-11 07:27:48 +0300

    Time: 2 ms (57.32%) | Memory: 7.4 MB (68.28%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -3,13 +3,8 @@ public:
     string convertToTitle(int columnNumber) {
         string res="";
         while(columnNumber){
-            if(columnNumber % 26){
-                res += string(1,char(columnNumber % 26 -1 +'A'));
-                columnNumber /=26;
-            }else{
-                res += "Z";
-                columnNumber =columnNumber /26 -1;
-            }
+            res += string(1,char((columnNumber-1) % 26 +'A'));
+            columnNumber =(columnNumber -1)/26;
         }
         reverse(res.begin(),res.end());
         return res;
