14c8e652296655a3f4feacdf24b11812d524d1cd
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-16 07:08:32 +0300

    Time: 25 ms (5.83%) | Memory: 17.4 MB (88.24%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        sort(timePoints.begin(),timePoints.end());
+        int s =timePoints.size();
+        vector<int> t(s);
+        for(int i=0;i<s;i++){
+            int h= stoi(timePoints[i].substr(0,2))*60;
+            int m= stoi(timePoints[i].substr(3,2));
+            t[i]=h+m;
+        }
+        int res=min(t[s-1]-t[0] ,24*60-(t[s-1]-t[0]));
+        for(int i=1;i<s;i++){
+            res = min({res ,t[i]-t[i-1] ,24*60-(t[i]-t[i-1])});
+        }
+
+        return res;
+    }
+};
