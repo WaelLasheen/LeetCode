189bc1a94524233ac12d553a068ac9f11b1fada6
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-17 02:09:19 +0300

    Time: 0 ms (100.00%) | Memory: 8.7 MB (41.05%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -5,6 +5,8 @@ public:
         for(int i=0;i<num.size()-2;i++){
             if(num[i]==num[i+1] && num[i]==num[i+2]){
                 res=max(res,num.substr(i,3));
+                // skip two next digits
+                i +=2;
             }
         }
 
