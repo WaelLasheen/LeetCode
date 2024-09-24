327a34f89359302497b021907bab0a6912c457fc
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-24 10:54:51 +0300

    Time: 179 ms (5.04%) | Memory: 74.7 MB (11.09%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n==1) return 1;
+        map<int,int> fr ,t;
+        for(auto i:trust){
+            fr[i[0]]++;
+            fr[i[1]]++;
+            t [i[0]]++;
+        }
+
+        for(auto i:fr){
+            if(i.second == n-1 && !t[i.first]){
+                return i.first;
+            }
+        }
+
+        return -1;
+    }
+};
