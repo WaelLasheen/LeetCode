9adf2814bf06c4e44f5d784ce5734bf2d02aa103
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-09-25 07:38:14 +0300

    Time: 2 ms (55.85%) | Memory: 8.2 MB (45.92%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     string makeGood(string s) {
         const int diff ='a'-'A';
-        string res="";
         stack<char>stack;
         for(char i:s){
             if(!stack.size()) stack.push(i);
             else if(abs(stack.top()-i)==diff) stack.pop();
             else stack.push(i);
         }
+        s="";
         while(stack.size()){
-            res += stack.top();
+            s += stack.top();
             stack.pop();
         }
-        reverse(res.begin(), res.end());
-        
-        return res;
+        reverse(s.begin(), s.end());
+
+        return s;
     }
 };
