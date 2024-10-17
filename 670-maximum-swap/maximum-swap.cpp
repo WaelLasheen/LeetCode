70586190fa14f4eb6e5b2f6e4539d11f80e8ba3e
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-10-17 07:09:12 +0300

    Time: 0 ms (100.00%) | Memory: 7.5 MB (89.76%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -3,16 +3,18 @@ public:
     int maximumSwap(int num) {
         string s= to_string(num);
         int len=s.size();
-        for(int i=0;i<len;i++){
-            char m=s[i];
-            int it=i;
-            for(int j=i+1;j<len;j++){
-                if(s[j]>=m){
-                    m=s[j];
-                    it=j;
-                }
+        vector<pair<char,int>> v(len,{s[len-1],len-1});
+        for(int i=len-2;i>=0;i--){
+            if(s[i]>v[i+1].first){
+                v[i]={s[i],i};
             }
-            if(m>s[i]){
+            else{
+                v[i]=v[i+1];
+            }
+        }
+        for(int i=0;i<len;i++){
+            if(s[i] !=v[i].first){
+                int it= v[i].second;
                 swap(s[i],s[it]);
                 break;
             }
