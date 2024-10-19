a630d7ec3af5982fb2b5e403f97b90c2c8e3c681
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-10-19 08:33:21 +0300

    Time: 0 ms (100.00%) | Memory: 14.9 MB (71.05%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> fr;
+        for (int i : nums) {
+            fr[i]++;
+        }
+        vector<pair<int, int>> pr;
+        for (auto i:fr) {
+            pr.push_back({i.second,-i.first});
+        }
+        sort(pr.begin(), pr.end());
+        int it=0;
+        for (auto i : pr) {
+            while(i.first--){
+                nums[it++]=-i.second;
+            }
+        }
+        return nums;
+    }
+};
