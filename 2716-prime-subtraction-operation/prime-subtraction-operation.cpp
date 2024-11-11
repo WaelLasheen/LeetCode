c1bb0e823595bea83a4c3f1034b5e7bd93df6be0
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-11-11 06:00:22 +0200

    Time: 8 ms (34.71%) | Memory: 29.3 MB (24.28%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -15,8 +15,8 @@ public:
         for(int i=nums.size()-2;i>-1;i--){
             if(nums[i] >= nums[i+1]){
                 // remove 1 and use upper bound or add 1 and use lower bound
-                int diff = nums[i] - nums[i+1];
-                auto it = upper_bound(prim.begin(),prim.end(),diff);
+                int diff = nums[i] - nums[i+1]+1;
+                auto it = lower_bound(prim.begin(),prim.end(),diff);
                 if(it == prim.end()) return false;
                 nums[i] -= *it;
                 if(nums[i] <= 0) return false;
