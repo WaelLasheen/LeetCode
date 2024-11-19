3d30d332d26631a17380c2f4830bf90e3ecb2cae
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-11-19 05:51:12 +0200

    Time: 105 ms (77.16%) | Memory: 98.3 MB (49.04%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -3,24 +3,21 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         ll res=0 ,sum=0;
         // we will use variable sliding window, but with constrain on its size
         int l=0 ,r=0;
         while(r<nums.size()){
-            if(!mp[nums[r]]){
-                sum += nums[r];
-                mp[nums[r]]++;
-            }
-            else{
+            if(mp[nums[r]]){
                 while(l<=r && mp[nums[r]]){
                     mp[nums[l]]--;
                     sum -= nums[l];
                     l++;
                 }
-                mp[nums[r]]++;
-                sum += nums[r];
             }
+
+            mp[nums[r]]++;
+            sum += nums[r];
 
             if(r-l+1 ==k){
                 res =max(res,sum);
