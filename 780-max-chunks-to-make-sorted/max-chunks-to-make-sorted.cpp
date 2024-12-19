2fd108f4a469c83997353347a805193473f5a729
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2024-12-19 10:34:32 +0200

    Time: 0 ms (100.00%) | Memory: 9.1 MB (42.82%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int res=0 ,md=0;    // max distance
+        for(int i=0;i<arr.size();i++){
+            md=max(md,arr[i]);
+            if(i==md){
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
