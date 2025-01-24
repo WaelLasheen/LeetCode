a48b001e3b090896f10f524540ae7ba5b44ec4bd
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2025-01-24 07:11:55 +0200

    Time: 10 ms (73.25%) | Memory: 51.8 MB (88.60%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> safe(n,-1); // -1:not visited ,0:false ,1:true
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            if(dfs(graph,safe,i)){
+                res.push_back(i);
+            }
+        }
+
+        return res;
+    }
+private:
+    bool dfs(vector<vector<int>>& graph ,vector<int>& safe ,int i){
+        if(safe[i]!=-1){
+            return safe[i];
+        }
+
+        safe[i]=0;
+        for(int next:graph[i]){
+            if(!dfs(graph,safe,next)){
+                return false;
+            }
+        }
+        safe[i]=1;
+        return true;
+    }
+};
