422fb628473e1e1bf1e1418f59831dcccff2fc76
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2025-02-09 08:31:22 +0200

    Time: 83 ms (20.21%) | Memory: 92.6 MB (19.30%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -6,14 +6,13 @@ public:
         // count good pair: j-i == nums[j]-nums[i]
         //  |--> i == nums[i]+shift
         //  |--> same shift is good pair
-        fastIO();   // make code faster
 
         unordered_map<long,int> mp;
         ll s = nums.size();
         for(long i=0;i<s;i++){
             mp[nums[i]-i]++;
         }
-
+        
         ll gp=0;    // good pairs
         for(auto i:mp){
             ll n=i.second;
@@ -21,12 +20,5 @@ public:
         }
 
         return s*(s-1)/2 - gp;
-    }
-
-private:
-    void fastIO(void) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
     }
 };
