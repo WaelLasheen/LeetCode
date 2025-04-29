46a7de9238faf1ab08b820e2479ea8bad520539a
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2025-04-29 11:41:02 +0300

    Time: 2 ms (30.96%) | Memory: 16 MB (15.82%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -2,52 +2,53 @@ class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         int r=board.size() ,c=board[0].size();
-        vector<vector<bool>> v(r,vector<bool>(c,false));
+        vector<vector<char>> res(r,vector<char>(c,'X'));
+        for(int it=0;it<c;it++){
+            if(board[0][it]=='O'){
+                connect(board,res,0,it);
+            }
+
+            if(board[r-1][it]=='O'){
+                connect(board,res,r-1,it);
+            }
+        }
+
+        for(int it=0;it<r;it++){
+            if(board[it][0]=='O'){
+                connect(board,res,it,0);
+            }
+
+            if(board[it][c-1]=='O'){
+                connect(board,res,it,c-1);
+            }
+        }
+
+        board=res;
+    }
+
+private:
+    void connect(vector<vector<char>>& board ,vector<vector<char>>& res ,int i ,int j){
         vector<vector<int>> dir={{-1,0},{1,0},{0,-1},{0,1}};
-
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){  
-                if(v[i][j] || board[i][j] =='X') continue;
-
-                int row=i ,col=j;
-                bool change= true;
-                v[i][j]=true;
-                queue<pair<int,int>> q ,memory;
-                q.push({row,col});
-                memory.push({row,col});
-                while(q.size()){
-                    row= q.front().first;
-                    col= q.front().second;
-                    q.pop();
-                    for(auto d:dir){
-                        if(!outOfRange(r,c,row+d[0],col+d[1]) && board[row+d[0]][col+d[1]]=='O' && !v[row+d[0]][col+d[1]]){
-                            v[row+d[0]][col+d[1]] =true;
-                            q.push({row+d[0] ,col+d[1]});
-                            if(change) memory.push({row+d[0] ,col+d[1]});
-                        }
-                    }
-
-                    if(change && atEdge(r,c,row,col)){
-                        change=false;
-                    }
-                }
-
-                while(change && memory.size()){
-                    int a= memory.front().first;
-                    int b= memory.front().second;
-                    board[a][b]='X';
-                    memory.pop();
+        int r=board.size() ,c=board[0].size();
+        board[i][j]='X';
+        res[i][j]='O';
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        while(q.size()){
+            i= q.front().first;
+            j= q.front().second;
+            q.pop();
+            for(auto d:dir){
+                if(!outOfRange(r,c,i+d[0],j+d[1]) && board[i+d[0]][j+d[1]] !='X'){
+                    board[i+d[0]][j+d[1]] ='X';
+                    res[i+d[0]][j+d[1]]='O';
+                    q.push({i+d[0] ,j+d[1]});
                 }
             }
         }
     }
 
-private:
     bool outOfRange(int r ,int c ,int i ,int j){
         return i<0 || j<0 || i==r || j==c;
-    }
-
-    bool atEdge(int r ,int c ,int i ,int j){
-        return !i || !j || i==r-1 || j==c-1;
     }
 };
