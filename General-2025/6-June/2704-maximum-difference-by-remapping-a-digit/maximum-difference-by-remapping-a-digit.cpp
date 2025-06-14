40dd3fcf2e55f6c0856231b89cd034a3a2a5e10e
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2025-06-14 09:32:31 +0300

    Time: 0 ms (100.00%) | Memory: 8.2 MB (64.15%) - LeetSync

--- General-2025/6-June/2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
+++ General-2025/6-June/2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int minMaxDifference(int num) {
         string mn=to_string(num);
-        for(int i=1;i<mn.size();i++){
-            if(mn[i]==mn[0]){
-                mn[i]='0';
-            }
-        }
-        mn[0]='0';
-
         string mx=to_string(num);
+
         int it=0;
         while(it+1<mx.size() && mx[it]=='9'){
             it++;
         }
-        for(int i=it+1;i<mx.size();i++){
-            if(mx[i]==mx[it]){
+
+        char mnd=mn[0] ,mxd=mx[it];
+
+        for(int i=0;i<mn.size();i++){
+            if(mn[i]==mnd){
+                mn[i]='0';
+            }
+            if(mx[i]==mxd){
                 mx[i]='9';
             }
         }
-        mx[it]='9';
 
         return stoi(mx)-stoi(mn);
     }
