abfce914ab886db79f532a67dea088a6df097bad
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2025-07-01 07:12:58 +0300

    Time: 0 ms (100.00%) | Memory: 8.8 MB (60.22%) - LeetSync

--- General-2025/6-June/3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ General-2025/6-June/3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        vector<int> poss;
-        int l=0 ,r=0;
+        int l=0 ,r=0 ,res=1;
         while(r<word.size()){
-            int fr=0;
             while(r<word.size() && word[l]==word[r]){
-                fr++;
                 r++;
             }
-            if(fr>1){
-                poss.push_back(fr-1);
+            if(r-l>1){
+                res += r-l-1;
             }
             l=r;
         }
-        int res= accumulate(poss.begin(),poss.end(),0)+1;
 
         return res;
     }
