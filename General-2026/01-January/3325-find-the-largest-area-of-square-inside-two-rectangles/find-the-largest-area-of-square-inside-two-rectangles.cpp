3cf91eb6f05b1a43a8de4d2e6a6afde196ee2fc3
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2026-01-17 10:13:10 +0200

    Time: 138 ms (14.05%) | Memory: 59.4 MB (56.20%) - LeetSync

--- General-2026/01-January/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ General-2026/01-January/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -1,17 +1,19 @@
 #define ll long long
 class Solution {
 public:
+    int delta(int s1 ,int s2 ,int e1 ,int e2){
+        int s = max(s1 ,s2);
+        int e= min(e1 ,e2);
+        return max(0,e-s);
+    }
+
     long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
         ll mxArea=0;
         int n=bottomLeft.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int x1 = max(bottomLeft[i][0] ,bottomLeft[j][0]);
-                int x2= min(topRight[i][0] ,topRight[j][0]);
-                int dx = max(0,x2-x1);
-                int y1 = max(bottomLeft[i][1] ,bottomLeft[j][1]);
-                int y2= min(topRight[i][1] ,topRight[j][1]);
-                int dy = max(0,y2-y1);
+                int dx = delta(bottomLeft[i][0] ,bottomLeft[j][0],topRight[i][0] ,topRight[j][0]);
+                int dy = delta(bottomLeft[i][1] ,bottomLeft[j][1],topRight[i][1] ,topRight[j][1]);
                 int side = min(dx,dy);
                 ll area = 1LL *side*side;
                 mxArea = max(mxArea ,area);
