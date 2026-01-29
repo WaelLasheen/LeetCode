3eb1447a0ec1f02826d50f1392f5527d42839aa8
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2026-01-29 11:21:03 +0200

    Time: 614 ms (8.09%) | Memory: 252.2 MB (5.08%) - LeetSync

--- General-2026/01-January/3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ General-2026/01-January/3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -5,7 +5,7 @@ public:
     ll minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         int n = original.size();
         vector<vector<int>> changeCost(26,vector<int>(26,INT_MAX));
-        vector<vector<int>> dp(26,vector<int>(26,INT_MAX));
+        vector<vector<ll>> dp(26,vector<ll>(26,INT_MAX));
         vector<unordered_set<int>> adj(26);
         ll res=0;
 
@@ -21,7 +21,6 @@ public:
             priority_queue<pair<ll,int> ,vector<pair<ll,int>>, greater<pair<ll,int>>> pq;
             vector<bool> v(26,false);
             pq.push({0,source[i]-'a'});
-            bool success = false;
 
             if(dp[source[i]-'a'][target[i]-'a'] != INT_MAX){
                 res += dp[source[i]-'a'][target[i]-'a'];
@@ -32,13 +31,7 @@ public:
                 ll tw = pq.top().first;
                 int u = pq.top().second;
                 pq.pop();
-
-                if(u == target[i]-'a'){
-                    res += tw;
-                    success = true;
-                    dp[source[i]-'a'][target[i]-'a'] = tw;
-                    break;
-                }
+                dp[source[i]-'a'][u] = min(dp[source[i]-'a'][u] ,tw);
                 
                 if(!v[u]){
                     for(int c:adj[u]){
@@ -50,7 +43,9 @@ public:
                 }
             }
 
-            if(!success) return -1;
+            if(dp[source[i]-'a'][target[i]-'a'] == INT_MAX) return -1;
+
+            res += dp[source[i]-'a'][target[i]-'a'];
         }
 
         return res;
