d8d9bc1be8552fd378ada7bef58632e26347701b
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2026-08-06 11:00:09 +0300

    Time: 0 ms (100.00%) | Memory: 8.7 MB (37.77%) - LeetSync

---
+++ General-2026/06-August/3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        while(true){
+            int x=n,p=1;
+            while(x){
+                p *= x%10;
+                x /=10;
+            }
+            if(p%t==0){
+                return n;
+            }
+            n++;
+        }
+
+            return -1; // any val
+    }
+};
