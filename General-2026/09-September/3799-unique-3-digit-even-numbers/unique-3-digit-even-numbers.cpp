e992aee706684384753717ff6856a92f014f37c7
Wael Lasheen <120112266+WaelLasheen@users.noreply.github.com>
2026-09-11 11:28:16 +0300

    Time: 0 ms (100.00%) | Memory: 28.7 MB (93.75%) - LeetSync

--- General-2026/09-September/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ General-2026/09-September/3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int> nums;
-        for(int i=0;i<digits.size();i++){
-            for(int j=0;j<digits.size();j++){
-                for(int k=0;k<digits.size();k++){
-                    if(i!=j && j!=k && i!=k && digits[i] && !(digits[k]&1)){
-                        nums.insert(digits[i]*100+digits[j]*10+digits[k]);
+        vector<int> freq(10, 0);
+        int count = 0;
+        for (int d : digits) freq[d]++;
+
+        for (int d1 = 1; d1 <= 9; d1++) {
+            if (freq[d1] == 0) continue;
+            freq[d1]--; 
+            for (int d2 = 0; d2 <= 9; d2++) {
+                if (freq[d2] == 0) continue;
+                freq[d2]--;
+                for (int d3 = 0; d3 <= 8; d3 += 2) {
+                    if (freq[d3] > 0) {
+                        count++;
                     }
                 }
+                freq[d2]++;
             }
+            freq[d1]++;
         }
 
-        return nums.size();
+        return count;
     }
 };
